843f488853155f76ac1793e0e7a101dd35c92885
Marcelo Pasut <pasutmarcelo@gmail.com>
2019-01-11 00:16:50 -0300

    destruyo el swarm cuando destruyo la infraestructura

--- local/cluster.tf
+++ local/cluster.tf
@@ -2,11 +2,11 @@ resource "null_resource" "create_cluster" {
 	provisioner "local-exec" {
 		command = "docker swarm init"
 	}
-}
-
-resource "null_resource" "show_nodes" {
-	depends_on = ["null_resource.create_cluster"]
 	provisioner "local-exec" {
 		command = "docker node ls"
+	}
+	provisioner "local-exec" {
+		command = "docker swarm leave -f"
+		when = "destroy"
 	}	
 }
