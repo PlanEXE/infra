fe68d06522bed633082e362afcc8f9b7990ff978
Marcelo Pasut <pasutmarcelo@gmail.com>
2019-01-12 15:04:08 -0300

    instalo portainers y visualizer en el cluester

--- local/cluster.tf
+++ local/cluster.tf
@@ -5,6 +5,10 @@ resource "null_resource" "create_cluster" {
 	provisioner "local-exec" {
 		command = "docker node ls"
 	}
+
+	provisioner "local-exec" {
+		command = "docker stack deploy -c ./stacks/tools-docker-composer.yml tools"
+	}
 	provisioner "local-exec" {
 		command = "docker swarm leave -f"
 		when = "destroy"
